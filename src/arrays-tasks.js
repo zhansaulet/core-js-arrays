a74f437035361f9258d3f230ae58d0f288a26673
Zhansaule Telisheva <jansaule97@mail.ru>
2024-02-12 23:37:34 +0500

    feat: add solution of get indicees of elements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -401,8 +401,9 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  // throw new Error('Not implemented');
+  return indices.reduce((a, i) => a[i], arr);
 }
 
 /**
